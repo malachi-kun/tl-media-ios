dda4cdd17dca172fa9c1eaac09317d05d734c732
Malachi Hul <malachihul@malachis-macbook-pro-2.local>
2017-12-14 11:39:59 +0900

    commit good build before implementing transition and audio features.

--- tl-media-ios/Features/Home/Header/HomeHeader.swift
+++ tl-media-ios/Features/Home/Header/HomeHeader.swift
@@ -1,42 +1,43 @@
+////
+////  HomeHeader.swift
+////  tl-media-ios
+////
+////  Created by Malachi Hul on 2017/11/20.
+////  Copyright © 2017 Tabi-Labo. All rights reserved.
+////
 //
-//  HomeHeader.swift
-//  tl-media-ios
+//import UIKit
 //
-//  Created by Malachi Hul on 2017/11/20.
-//  Copyright © 2017 Tabi-Labo. All rights reserved.
 //
+//class HomeHeader:UICollectionViewCell {
+//
+//    // MARK: UI ELEMENTS
+//    var headerImage:UIImageView = {
+//        let iv = UIImageView()
+//        iv.image = UIImage(named: "TL")  //fileName : default image
+//        iv.contentMode = .scaleAspectFit
+//        iv.backgroundColor = .black
+//        return iv
+//    }()
+//
+//    // MAARK: PROPERTIES
+//    var imageURL:String?
+//
+//    // MARK: LIFECYCLE CODE
+//    override init(frame: CGRect){
+//        super.init(frame: frame)
+//         setupUI()
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//
+//    // MARK: HELPER METHODS
+//    private func setupUI(){
+//        addSubview(headerImage)
+//
+//        headerImage.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+//    }
+//}
 
-import UIKit
-
-
-class HomeHeader:UICollectionViewCell {
-    
-    // MARK: UI ELEMENTS
-    var headerImage:UIImageView = {
-        let iv = UIImageView()
-        iv.image = UIImage(named: "TL")  //fileName : default image
-        iv.contentMode = .scaleAspectFit
-        iv.backgroundColor = .black
-        return iv
-    }()
-    
-    // MAARK: PROPERTIES
-    var imageURL:String?
-    
-    // MARK: LIFECYCLE CODE
-    override init(frame: CGRect){
-        super.init(frame: frame)
-         setupUI()
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    // MARK: HELPER METHODS
-    private func setupUI(){
-        addSubview(headerImage)
-        
-        headerImage.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
-    }
-}
